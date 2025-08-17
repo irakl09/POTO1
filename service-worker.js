681b31dcd98979032a18b644b2abcd7da15aa031
irakl09 <143933058+irakl09@users.noreply.github.com>
2025-08-17 09:05:23 +0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,39 +3,17 @@ const urlsToCache = [
   '/',
   '/index.html',
   '/m.jpg',
-  '/m-512.jpg',
-  '/manifest.json',
-  '/js/jspdf.umd.min.js'
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache).catch(err => {
-        console.error('Cache addAll error:', err);
-      });
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request).catch(() => {
-        return caches.match('/index.html');
-      });
-    })
-  );
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames
-          .filter(name => name !== CACHE_NAME)
-          .map(name => caches.delete(name))
-      );
-    })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
